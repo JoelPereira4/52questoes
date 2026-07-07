c061e8db3a2a4d5658e9894632719838da0eaa99
JoelPereira4 <jp3775454@gmail.com>
2026-07-07 01:51:01 -0300

    Add program to find the successor of a number q5

---
+++ questao5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("DIGITE SEU NUMERO:\n");
+    scanf("%d", &num);
+    printf("O SUCESSOR DO SEU NUMERO É:%d\n", num+1);
+
+    return 0;
+}
