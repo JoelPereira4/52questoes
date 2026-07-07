c63f4267e8b24f1c108907413715c54e3aeb1468
JoelPereira4 <jp3775454@gmail.com>
2026-07-07 01:52:22 -0300

    Add program to square a user-input number

---
+++ questoa7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("DIGITE SEU NUMERO:\n");
+    scanf("%d", &num);
+    printf("O SEU NUMERO AO QUADRADO É:%d\n", num*num);
+
+    return 0;
+} 
