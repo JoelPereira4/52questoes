751a9fc31ceade82ad4206b22cb7f505db300b93
JoelPereira4 <jp3775454@gmail.com>
2026-07-07 01:59:59 -0300

    QUESTAO13

---
+++ Q13.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int vari;
+
+    printf("DIGITE SEU NUMERO:\n");
+    scanf("%d", &vari);
+
+        for (int i = 1; i <= 5; i++) {
+            printf("%d\n", vari + 1 * i);
+        }
+       }
