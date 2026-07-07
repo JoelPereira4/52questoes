9a8e339ab580b5030c5a31b8e2ed33289d8e29d6
JoelPereira4 <jp3775454@gmail.com>
2026-07-07 02:23:07 -0300

    QUESTAO45

---
+++ Q45.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int primeiro, ultimo, razao;
+    int soma = 0;
+
+    printf("Primeiro termo: ");
+    scanf("%d", &primeiro);
+
+    printf("Ultimo termo: ");
+    scanf("%d", &ultimo);
+
+    printf("Razao: ");
+    scanf("%d", &razao);
+
+    for(int i = primeiro; i <= ultimo; i += razao) {
+        soma += i;
+    }
+
+    printf("Soma da PA = %d\n", soma);
+
+    return 0;
+}
