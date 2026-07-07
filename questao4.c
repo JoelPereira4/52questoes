5da1a75b68bd1a2e9d2008eed43cf20409cb852a
JoelPereira4 <jp3775454@gmail.com>
2026-07-07 01:50:13 -0300

    questao4.c

---
+++ questao4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    int num2;
+    int soma;
+    printf("DIGITE SEU PRIMEIRO NUMERO:\n");
+    scanf("%d", &num);
+    printf("DIGITE SEU SEGUNDO NUMERO:\n");
+    scanf("%d", &num2);
+    soma=num*num2;
+    printf("A MULTIPLICAÇÃO ENTRE OS NUMEROS É:%d\n", soma);
+
+    return 0;
+}
