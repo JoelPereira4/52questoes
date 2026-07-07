19fbbbe64ca3119dd462a74263f9a80ce2be73ff
JoelPereira4 <jp3775454@gmail.com>
2026-07-07 02:10:44 -0300

    QUESTAO27

---
+++ Q27.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    do {
+        printf("Digite N: ");
+        scanf("%d", &N);
+
+        if (N == 0)
+            break;
+
+        int soma = 0;
+        int impar = 1;
+        int termos = 0;
+
+        while (soma < N) {
+            soma += impar;
+            impar += 2;
+            termos++;
+        }
+
+        if (soma == N)
+            printf("Raiz quadrada = %d\n", termos);
+        else
+            printf("Nao eh quadrado perfeito\n");
+
+    } while (1);
+
+    return 0;
+}
